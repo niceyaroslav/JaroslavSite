e6c557a9ce4167c2c3a99a33ffbbbdbb87e54bd7
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-04-09 18:59:59 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+function handlePopup(name) {
+    let popup = document.getElementById(name);
+    popup.classList.replace("hidden", "show");
+}
+
+function closePopup(name) {
+    let popup = document.getElementById(name);
+    popup.classList.replace("show", "hidden");
+}
+
+function hamburgerHandler() {
+    let x = document.getElementById("hamburger");
+    x.classList.toggle("responsive");
+    let icon = document.getElementById('icon');
+    if (icon.innerHTML.includes('<i class="fa fa-bars"></i>')) {
+        icon.innerHTML = '<i class="fa fa-times"></i>';
+    } else {
+        icon.innerHTML = '<i class="fa fa-bars"></i>';
+    }
+}
+
